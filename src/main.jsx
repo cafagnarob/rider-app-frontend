d069df56353e68b97e4bb01d3046f4486da4802f
cafagnarob <cafagna.rob@gmail.com>
2026-07-30 18:05:35 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,14 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import { store } from "./app/store.js"
 
-createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+)
