1ad7cca39c96e7e206ad89ed32151c16a1b39ee4
cafagnarob <cafagna.rob@gmail.com>
2026-07-30 20:19:50 +0200

    Update authSlice.js

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -5,11 +5,9 @@ const authSlice = createSlice({
   initialState: { user: null, token: null },
   reducers: {
     setCredentials: (state, action) => {
-      state.user = action.payload.user
       state.token = action.payload.token
     },
     logout: (state) => {
-      state.user = null
       state.token = null
     },
   },
