972afe373fb8387d013e5a261974e0b5e6bced77
cafagnarob <cafagna.rob@gmail.com>
2026-07-30 20:20:13 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react"
+import { useLoginMutation } from "../features/auth/authApi"
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { setCredentials } from "../features/auth/authSlice"
+
 function LoginPage() {
-  return <h1> LOGIN </h1>
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [login, { isLoaging, error }] = useLoginMutation()
+  const dispach = useDispatch()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const result = await login({ username, password }).unwrap()
+      dispach(setCredentials({ token: result.accessToken }))
+      navigate("/")
+    } catch (err) {
+      console.error("login fallito", err)
+    }
+
+    return
+  }
 }
 export default LoginPage
