22a2a4de841efb7104cbfb9d8bc7cf592eb62bdb
cafagnarob <cafagna.rob@gmail.com>
2026-07-31 13:15:54 +0200

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,10 @@ import Layout from "../components/layout/Layout"
 import HomePage from "../pages/HomePage"
 import LoginPage from "../pages/LoginPage"
 import EventsPage from "../pages/EventsPage"
+import ForgotPasswordPage from "../pages/ForgotPasswordPage"
+import ResetPasswordPage from "../pages/ResetPasswordPage"
+import VerifyEmailPage from "../pages/VerifyEmailPage"
+import RegisterPage from "../pages/RegisterPage"
 
 function AppRoutes() {
   return (
@@ -10,8 +14,12 @@ function AppRoutes() {
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/events" element={<EventsPage />} />
-        <Route path="/login" element={<LoginPage />} />
       </Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/verify-email" element={<VerifyEmailPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   )
 }
