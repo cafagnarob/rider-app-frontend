553f51e70d674fe58e6de34c5f5cee46d33639bb
cafagnarob <cafagna.rob@gmail.com>
2026-07-31 15:46:26 +0200

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,20 +1,68 @@
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { useGetCurrentUserQuery } from "../../features/users/usersApi"
+import { logout } from "../../features/auth/authSlice"
+import { Container, Dropdown, Nav, Navbar as BsNavbar } from "react-bootstrap"
 
 function Navbar() {
+  const token = useSelector((state) => state.auth.token)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { data: currentUser } = useGetCurrentUserQuery(undefined, {
+    skip: !token,
+  })
+
+  const handleLogout = () => {
+    dispatch(logout())
+    navigate("/login")
+  }
+
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-dark px-3">
-      <Link className="navbar-brand" to="/">
-        Rider App
-      </Link>
-      <div className="navbar-nav">
-        <Link className="nav-link" to="/events">
-          Eventi
-        </Link>
-        <Link className="nav-link" to="/login">
-          Login
-        </Link>
-      </div>
-    </nav>
+    <BsNavbar expand="lg" variant="dark" bg="dark">
+      <Container>
+        <BsNavbar.Brand as={Link} to="/">
+          Rider App
+        </BsNavbar.Brand>
+        <BsNavbar.Toggle aria-controls="main-nav" />
+        <BsNavbar.Collapse id="main-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/events">
+              Eventi
+            </Nav.Link>
+          </Nav>
+
+          <Nav>
+            {token ? (
+              <Dropdown align="end">
+                <Dropdown.Toggle variant="dark" id="user-menu">
+                  {currentUser?.username || "Account"}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item as={Link} to="/profile">
+                    Profilo
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/garage">
+                    Garage
+                  </Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item onClick={handleLogout}>Esci</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Accedi
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Registrati
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </BsNavbar.Collapse>
+      </Container>
+    </BsNavbar>
   )
 }
 export default Navbar
