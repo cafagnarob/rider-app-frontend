5082d0aafb7d8e4f150934958fe362ed6d0bcd43
cafagnarob <cafagna.rob@gmail.com>
2026-07-31 22:59:52 +0200

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const CATEGORY_LABELS = {
+  NAKED: "Naked",
+  ADVENTURE: "Adventure",
+  SPORT_TOURING: "Sport Touring",
+  ENDURO: "Enduro",
+  CRUISER: "Cruiser",
+  SPORT: "Sportiva",
+  TOURING: "Touring",
+  CUSTOM: "Custom",
+  SCOOTER: "Scooter",
+}
