6d03a8c6d1346467d677df896029abce4d4e4a67
cafagnarob <cafagna.rob@gmail.com>
2026-07-31 12:06:02 +0200

    Update authSlice.js

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,14 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const tokenFromStorage = localStorage.getItem("token")
+
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null },
+  initialState: { token: tokenFromStorage || null },
   reducers: {
     setCredentials: (state, action) => {
       state.token = action.payload.token
+      if (action.payload.remember) {
+        localStorage.setItem("token", action.payload.token)
+      }
     },
     logout: (state) => {
       state.token = null
+      localStorage.removeItem("token")
     },
   },
 })
