a182a72b21f673b1e853bdcd184d76716c73bea7
cafagnarob <cafagna.rob@gmail.com>
2026-08-04 11:31:52 +0200

    Update usersApi.js

--- src/features/users/usersApi.js
+++ src/features/users/usersApi.js
@@ -6,6 +6,47 @@ export const usersApi = apiSlice.injectEndpoints({
       query: () => "/users/me",
       providesTags: ["User"],
     }),
+    selectVehicle: builder.mutation({
+      query: (vehicleId) => ({
+        url: `/users/me/vehicle/${vehicleId}`,
+        method: "PATCH",
+      }),
+      invalidatesTags: ["User"],
+    }),
+    clearVehicle: builder.mutation({
+      query: () => ({
+        url: "/users/me/vehicle",
+        method: "DELETE",
+      }),
+      invalidatesTags: ["User"],
+    }),
+    updateProfile: builder.mutation({
+      query: (body) => ({
+        url: "/users/me",
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    updateProfilePicture: builder.mutation({
+      query: (file) => {
+        const formData = new FormData()
+        formData.append("file", file)
+        return {
+          url: "/users/me/picture",
+          method: "PATCH",
+          body: formData,
+          responseHandler: "text",
+        }
+      },
+      invalidatesTags: ["User"],
+    }),
   }),
 })
-export const { useGetCurrentUserQuery } = usersApi
+export const {
+  useGetCurrentUserQuery,
+  useSelectVehicleMutation,
+  useClearVehicleMutation,
+  useUpdateProfileMutation,
+  useUpdateProfilePictureMutation,
+} = usersApi
