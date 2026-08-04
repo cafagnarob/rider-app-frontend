a70479ec3d0d1faa5d0692a0dba230ca3121847d
cafagnarob <cafagna.rob@gmail.com>
2026-08-04 10:09:52 +0200

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,3 +9,11 @@ export const CATEGORY_LABELS = {
   CUSTOM: "Custom",
   SCOOTER: "Scooter",
 }
+
+export const CC_RANGES = [
+  { value: "", label: "Tutte le cilindrate" },
+  { value: "0-400", label: "Fino a 400 cc" },
+  { value: "401-700", label: "400 – 700 cc" },
+  { value: "701-1000", label: "700 – 1000 cc" },
+  { value: "1001-", label: "Oltre 1000 cc" },
+]
