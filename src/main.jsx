18dcc6df6cfe805f31dbbdc592473a6bbbfaec8e
cafagnarob <cafagna.rob@gmail.com>
2026-08-10 13:07:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { store } from "./app/store.js"
-import { ThemeProvider } from "styled-components"
-import { theme } from "./styles/theme"
-import { GlobalStyle } from "./styles/GlobalStyle"
+import "./styles/shared.css"
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </Provider>,
 )
