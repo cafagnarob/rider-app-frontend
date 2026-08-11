c4097ac5a27666c20b44fddd0fb5ee84f8fabc74
cafagnarob <cafagna.rob@gmail.com>
2026-08-11 14:51:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { store } from "./app/store.js"
